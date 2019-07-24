c6b7167af625df8bf0b1415febc47b40b5516a06
davisbarreira <davisbarreira@gmail.com>
2019-07-23 21:48:27 -0300

    Criando o form em flask

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from forms import AuthorForm
+
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'iahuq3#%1u982hFA)#($mx'
 
 @app.route('/')
 def index():
     return render_template('home.html')
     # return '<h1>Hello</h1>'
+
+
+
+@app.route('/test',methods=['GET','POST'])
+def test():
+    form = AuthorForm()
+    print(form.authorname.data)
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+    return render_template('test.html',form=form)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host='0.0.0.0')
